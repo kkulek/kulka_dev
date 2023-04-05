5359ce08778bf2b830eb0f3f910a9153f95e6532
kkulek <106318909+kkulek@users.noreply.github.com>
2023-04-05 18:49:30 +0200

    added content and styling for the footer

--- src/components/reusables/ButtonReact.jsx
+++ src/components/reusables/ButtonReact.jsx
@@ -7,7 +7,7 @@ export default function ButtonReact({ href, primary, blank, children }) {
 			className={
 				primary
 					? "py-1 px-4 bg-black text-white text-base border border-black transition-color duration-500"
-					: "py-1 px-4 bg-white text-black border border-black text-base transition-color duration-500"
+					: "py-1 px-4 bg-transparent text-inherit border border-gray-700 text-base transition-color duration-500"
 			}
 			target={blank && "_blank"}
 		>
